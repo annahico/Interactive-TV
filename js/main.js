e21f31c190969722c25b95fef58757aa0ee5c43f
annahico <annahico@gmail.com>
2024-04-26 20:00:22 +0200

    añadidos comentarios tecnicos de cada funcion

--- js/main.js
+++ js/main.js
@@ -1,15 +1,15 @@
-const buttons = document.getElementsByClassName("button");
-const offScreen = document.getElementById("offScreen");
-const indexContent = document.getElementById("indexContent");
-const powerBtn = document.getElementById("onOff");
-const selectedChannel = document.getElementsByClassName("selectedChannel");
-const clock = document.querySelector(".clockAndDate");
+const buttons = document.getElementsByClassName("button"); //Esta línea selecciona todos los elementos HTML que tienen la clase "button" y los almacena en una variable llamada buttons.
+const offScreen = document.getElementById("offScreen"); //Esto selecciona un elemento HTML con el id "offScreen" y lo almacena en la variable offScreen.
+const indexContent = document.getElementById("indexContent"); //Similar al paso anterior, esto selecciona un elemento con el id "indexContent" y lo guarda en la variable indexContent.
+const powerBtn = document.getElementById("onOff"); //De manera análoga a los pasos anteriores, esto selecciona un elemento con el id "onOff" y lo asigna a la variable powerBtn.
+const selectedChannel = document.getElementsByClassName("selectedChannel"); //Igual que en el primer paso, esta línea selecciona todos los elementos con la clase "selectedChannel" y los almacena en la variable selectedChannel.
+const clock = document.querySelector(".clockAndDate"); // Aquí se selecciona el primer elemento en el documento HTML que tiene la clase "clockAndDate" y se asigna a la variable clock.
 
-let Ontv = false;
+let Ontv = false; // Se declara una variable Ontv que se utiliza para rastrear si la televisión está encendida o apagada. Inicialmente, está establecida en false para indicar que la televisión está apagada.
 
-indexContent.style.display = "none";
+indexContent.style.display = "none"; // Esto oculta el elemento representado por indexContent estableciendo su propiedad de visualización (display) en "none".
 
-powerBtn.addEventListener("click", (e) => {
+powerBtn.addEventListener("click", (e) => { //Se agrega un "escuchador de eventos" al botón de encendido (powerBtn). Cuando se hace clic en este botón, se ejecuta una función que cambia el estado de Ontv (de encendido a apagado o viceversa) y realiza acciones correspondientes, como mostrar u ocultar contenido.
     Ontv = !Ontv;
 
     if (Ontv) {
@@ -23,7 +23,7 @@ powerBtn.addEventListener("click", (e) => {
     }
 });
 
-Array.from(buttons).forEach((item) => {
+Array.from(buttons).forEach((item) => { //Se itera sobre cada botón obtenido en la variable buttons. Por cada botón, se agrega un "escuchador de eventos" para manejar el clic del usuario.
     item.addEventListener("click", (event) => {
         if (Ontv) {
             const channelNumber = event.target.id.slice(-1);
@@ -40,7 +40,7 @@ Array.from(buttons).forEach((item) => {
     });
 });
 
-function getChannelName(channelNumber) {
+function getChannelName(channelNumber) { //Esta función toma un número de canal como entrada y devuelve el nombre del canal correspondiente. Utiliza una estructura de switch para asignar nombres a diferentes números de canal.
     switch (channelNumber) {
         case "1":
             return "Netflix";
@@ -55,7 +55,7 @@ function getChannelName(channelNumber) {
         case "6":
             return "Apple TV+";
         case "7":
-            return "Filming Channel";
+            return "Filming";
         case "8":
             return "Movistar+";
         case "9":
@@ -65,7 +65,7 @@ function getChannelName(channelNumber) {
     }
 }
 
-function updateDisplay(channelNumber, channelName) {
+function updateDisplay(channelNumber, channelName) { //Esta función actualiza la pantalla con el número y el nombre del canal seleccionado. Si la TV está apagada, simplemente limpia el contenido de la pantalla.
     const channelNumberElement = document.getElementById("channelNumber");
 
     if (channelNumber && channelName) {
@@ -75,7 +75,7 @@ function updateDisplay(channelNumber, channelName) {
     }
 }
 
-function updateClock() {
+function updateClock() { //Esta función actualiza el reloj en la pantalla con la hora actual y, si la TV está encendida, también muestra la fecha. Utiliza la función setInterval para actualizar el reloj cada segundo.
     const now = new Date();
     const hours = now.getHours().toString().padStart(2, "0");
     const minutes = now.getMinutes().toString().padStart(2, "0");
